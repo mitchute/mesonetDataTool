177c807a4760ce058ddfc8ab15cf378d448a358a
mmatts <matt.s.mitchell@okstate.edu>
2014-01-17 16:16:25 -0600

    Tried adding a menu bar.

--- src/ReadMesonet.py
+++ src/ReadMesonet.py
@@ -72,6 +72,27 @@ class PyApp(gtk.Window):
 		self.add(self.main_scroller)
 		# I'm not sure the difference between show and show_all, but maybe show_all initializes all the widgets?
 		self.show_all()
+		
+
+		mb = gtk.MenuBar()
+
+		filemenu = gtk.Menu()
+		filem = gtk.MenuItem("File")
+		filem.set_submenu(filemenu)
+
+		exit = gtk.MenuItem("Exit")
+		exit.connect("activate", gtk.main_quit)
+		filemenu.append(exit)
+
+		mb.append(filem)
+
+		vbox = gtk.VBox(False, 2)
+		vbox.pack_start(mb,False,False,0)
+
+		self.add(vbox)
+
+		
+
 
 	def get_mesonet_data(self):
 
